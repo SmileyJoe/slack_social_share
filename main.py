055529b364095687078257dd949ce340c9b5c32c
SmileyJoe <cody.strugnell@gmail.com>
2016-11-12 16:03:31 +0200

    Created slack object

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import twitter
 from pprint import pprint
 from settings import *
 import requests
+from slack import *
 from requests.utils import quote
 
 api = twitter.Api(consumer_key=TWITTER_CONSUMER_KEY,
@@ -10,42 +11,19 @@ api = twitter.Api(consumer_key=TWITTER_CONSUMER_KEY,
                   access_token_secret=TWITTER_ACCESS_TOKEN_SECRET)
 
 statuses = api.GetUserTimeline(screen_name=TWITTER_SCREEN_NAME)
-#print([s for s in statuses])
 
-#twitterUrl = "https://twitter.com/%s/status/%s?lang=en"
+slack = Slack()
+slack.token(SLACK_TOKEN)
+slack.channel(SLACK_CHANNEL)
+slack.unfurlMedia(False)
 
-slackBase = "https://slack.com/api/"
-slackChatPost = slackBase + "chat.postMessage?"
+for status in statuses:
+     if status.text.find(SHARE_HASH) != -1:
+        slack.text(status.text)
+        slack.icon(status.user.profile_image_url)
+        slack.userName(status.user.screen_name)
 
-slackAttachments = "&attachments=%s"
-slackAttachmentsImage = "{'image_url': '%s', 'fallback': 'test fallback'}"
-slackToken = "&token=" + SLACK_TOKEN
-slackChannel = "&channel=" + quote(SLACK_CHANNEL, safe='')
-slackPretty = "&pretty=1"
-slackUnfurlMedia = "&unfurl_media=false"
+        for media in status.media:
+             slack.image(media.media_url)
 
-slackUrl = slackChatPost + slackToken + slackChannel + slackPretty + slackUnfurlMedia
-
-for s in statuses:
-    if s.text.find(SHARE_HASH) != -1:
-        slackText = "&text=" + quote(s.text, safe='')
-        slackIcon = "&icon_url=" + quote(s.user.profile_image_url, safe="")
-        slackUsername = "&as_user=false&username=" + quote(s.user.screen_name, safe='')
-
-        slackUrl = slackUrl + slackText + slackIcon + slackUsername
-
-        first = True
-        images = ""
-        for m in s.media:
-            if first:
-                first = False
-            else:
-                images = images + ","
-            images = images + slackAttachmentsImage % (m.media_url)
-
-        if images:
-            slackExtras = slackAttachments % (quote("[" + images + "]", safe=""))
-            slackUrl = slackUrl + slackExtras
-        
-        response = requests.get(slackUrl)
-        pprint(response.content)
+        pprint(slack.postMessage())
